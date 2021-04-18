6c135f84b825626290067a1e4c6c75e64f27b877
Omair Khan <omairkhan064@gmail.com>
2021-04-19 02:36:15 +0530

    UPDATE- code refactor and session helpers

---
+++ utils/sessions.py
@@ -0,0 +1,48 @@
+import contextlib
+from functools import wraps
+from inspect import signature
+from typing import Callable
+
+
+# SqlAlchemy session helpers that can be used as a decorator
+
+@contextlib.contextmanager
+def create_session():
+    """Contextmanager that will create and teardown a session."""
+    session = settings.Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def find_session_idx(func):
+    """Find session index in function call parameter."""
+    func_params = signature(func).parameters
+    try:
+        session_args_idx = tuple(func_params).index("session")
+    except ValueError:
+        raise ValueError(f"Function {func.__qualname__} has no `session` argument") from None
+
+    return session_args_idx
+
+
+def provide_session(func):
+    """
+    Function decorator that provides a session if it isn't provided.
+    """
+    session_args_idx = find_session_idx(func)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if "session" in kwargs or session_args_idx < len(args):
+            return func(*args, **kwargs)
+        else:
+            with create_session() as session:
+                return func(*args, session=session, **kwargs)
+
+    return wrapper
