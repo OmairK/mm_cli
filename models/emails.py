8c55b66d221c402da57398de8f6618498d7b5685
Omair Khan <omairkhan064@gmail.com>
2021-04-19 20:51:13 +0530

    REFACTOR- Models file

--- models/emails.py
+++ models/emails.py
@@ -1,15 +1,21 @@
 from sqlalchemy import ARRAY, Column, Date, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from models.base import engine, Base, Session
+from configs.db_configs import DATABASE_URI
+
+engine = create_engine(DATABASE_URI)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
 
 
 class EMail(Base):
     """
     Stores the email metadata
     """
-
     __tablename__ = "email"
-    id = Column(String, primary_key=True)
+    message_id = Column(String, primary_key=True)
     sender = Column(String)
     recipient = Column(String)
     date = Column(Date)
